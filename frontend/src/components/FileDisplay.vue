ce1e53a3f796fc5e030a70b39d3e1b2456bd096e
Wenbin <1352075893@qq.com>
2017-08-09 21:12:32 +0800

    Add ppt show Ref #20

--- frontend/src/components/FileDisplay.vue
+++ frontend/src/components/FileDisplay.vue
@@ -1,14 +1,52 @@
 <template>
     <div class="file-display">
-        file display
+        <img :src="this.route" width="680px" />
+        <input type="button" value="pre" @click="prepicture" />
+        <input type="button" value="next" @click="nextpicture" />
     </div>
 </template>
 
+<script src="/socket.io/socket.io.js"></script>
 <script>
+import * as io from 'socket.io-client'
+
 export default {
     name: 'file-display',
     data: function () {
-        return {}
+        return {
+            roomid: '',
+            socket: '',
+            baseroute: '/static/img/',
+            recroute: '',
+            route: '',
+            maxnum: '',
+            nownum: ''
+        }
+    },
+    created: function () {
+        this.roomid = this.$route.params.id
+        this.recroute = 'ppt/shili-'
+        this.nownum = 1
+        this.maxnum = 15
+        this.route = this.baseroute+this.recroute+this.nownum+'.png'
+        console.log(this.route)
+    },
+    methods: {
+        nextpicture: function () {
+            if (this.nownum < this.maxnum) {
+                this.nownum = this.nownum + 1
+                this.route = this.baseroute+this.recroute+this.nownum+'.png'
+            }
+        },
+        prepicture: function () {
+            if (this.nownum > 1) {
+                this.nownum = this.nownum - 1
+                this.route = this.baseroute+this.recroute+this.nownum+'.png'
+            }
+        }
+    },
+    mounted: function () {
+        
     }
 }
 </script>
