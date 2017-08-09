5e04707815181ffde066abcac1edbfaaf6a98fc6
cocoon_yh <cocoon@mail.nankai.edu.cn>
2017-08-10 01:03:11 +0800

    Didnot finish.Can't set memu-position.Ref #22

--- frontend/src/components/ChatBoard.vue
+++ frontend/src/components/ChatBoard.vue
@@ -6,6 +6,23 @@
             <input id="msgInput" class="msgInput" autocomplete="off" />
             <button @click="sendMsg">Send</button>
         </form>
+        <Menu id="menu" mode="horizontal" :theme="theme1" active-name="1" style='display:none'>
+            <Submenu name="3">
+                <template slot="title">
+                    <Icon type="stats-bars"></Icon>
+                    统计分析
+                </template>
+                <Menu-group title="使用">
+                    <Menu-item name="3-1">新增和启动</Menu-item>
+                    <Menu-item name="3-2">活跃分析</Menu-item>
+                    <Menu-item name="3-3">时段分析</Menu-item>
+                </Menu-group>
+                <Menu-group title="留存">
+                    <Menu-item name="3-4">用户留存</Menu-item>
+                    <Menu-item name="3-5">流失用户</Menu-item>
+                </Menu-group>
+            </Submenu>
+        </Menu>
     </div>
 </template>
 
@@ -43,6 +60,67 @@ export default {
                 this.username = obj.name
             })
         }
+        // 监听
+        this.socket = io.connect('http://localhost:9000')
+        this.socket.emit('join', this.id + '.1')
+        this.socket.on('message', function (data) {
+            let ul = document.getElementById('messages')
+            let li = document.createElement('li')
+            // 创建button
+            let button = document.createElement('input')
+            button.type = 'button'
+            button.value = data['username'] + ' : ' + data['message']
+            let styleArr = ['background-color:Transparent;', 'border-style:None;', 'outline:none;',
+                'word-wrap: break-word !important;', 'word-break: break-all !important;',
+                'white-space: normal !important;', 'text-align:left;', 'padding-left:10px;',
+                'float:left;', 'padding-left:0px;', 'width:100%;']
+            let style = ''
+            for (let i = 0; i < styleArr.length; i++) {
+                style += styleArr[i]
+            }
+            // 如果判定是本人就给字体加粗 判断未生效
+            if (data.username === this.username) {
+                style += 'font-weight:bold;'
+            }
+            button.style = style
+            button.onmousedown = function (oEvent) {
+                let memu = document.getElementById('menu')
+                alert('ss')
+                // memu.style.display = 'block'
+                // memu.style.position = 'absolute'
+                // memu.style.left = oEvent.pageY
+                // memu.style.top = oEvent.pageX
+                memu.style = ('display:block;position:absolute;left:' + oEvent.pageX + ';top:' + oEvent.pageY + ';')
+                // // 如果是老师就有操作权限 判断未生效
+                // if (this.teacherName === this.username) {
+                //     if (!oEvent) {
+                //         oEvent = window.event
+                //     }
+                //     if (oEvent.button === 2) {
+                //         alert('您将禁言用户： ' + data['username'])
+                //         fetch('gag', {
+                //             method: 'post',
+                //             mode: 'cors',
+                //             headers: {
+                //                 'Content-Type': 'application/json, text/plain, */*',
+                //                 'Accept': 'application/json'
+                //             },
+                //             body: JSON.stringify({
+                //                 'name': data['username'],
+                //                 'roomID': this.id
+                //             })
+                //         }).then((response) => response.json()).then((obj) => { })
+                //     }
+                // }
+            }
+            // 屏蔽右键菜单
+            button.oncontextmenu = function () {
+                return false
+            }
+            li.appendChild(button)
+            ul.insertBefore(li, ul.childNodes[ul.childNodes.length])
+            ul.scrollTop = ul.scrollHeight
+        })
     },
     methods: {
         sendMsg: function () {
@@ -70,62 +148,6 @@ export default {
                 }
             })
         }
-    },
-    mounted: function () {
-        // 先获取ID 这个函数在父组件与子组件之间传递数据之前被调用
-        let id = this.$route.params.id
-        // 监听
-        this.socket = io.connect('http://localhost:9000')
-        this.socket.emit('join', this.id + '.1')
-        this.socket.on('message', function (data) {
-            let ul = document.getElementById('messages')
-            let li = document.createElement('li')
-            // 创建button
-            let button = document.createElement('input')
-            button.type = 'button'
-            button.value = data['username'] + ' : ' + data['message']
-            let styleArr = ['background-color:Transparent;', 'border-style:None;', 'outline:none;',
-                'word-wrap: break-word !important;', 'word-break: break-all !important;',
-                'white-space: normal !important;', 'text-align:left;', 'padding-left:10px;',
-                'float:left;', 'padding-left:0px;', 'width:100%;']
-            let style = ''
-            for (let i = 0; i < styleArr.length; i++) {
-                style += styleArr[i]
-            }
-            // 如果判定是本人就给字体加粗
-            style += 'font-weight:bold;'
-            button.style = style
-            button.onmousedown = function (oEvent) {
-                // 此处判定未生效
-                if (this.teacherName === this.username) {
-                    if (!oEvent) {
-                        oEvent = window.event
-                    }
-                    if (oEvent.button === 2) {
-                        alert('您将禁言用户： ' + data['username'])
-                        fetch('gag', {
-                            method: 'post',
-                            mode: 'cors',
-                            headers: {
-                                'Content-Type': 'application/json, text/plain, */*',
-                                'Accept': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                'name': data['username'],
-                                'roomID': id
-                            })
-                        }).then((response) => response.json()).then((obj) => { })
-                    }
-                }
-            }
-            // 屏蔽右键菜单
-            button.oncontextmenu = function () {
-                return false
-            }
-            li.appendChild(button)
-            ul.insertBefore(li, ul.childNodes[ul.childNodes.length])
-            ul.scrollTop = ul.scrollHeight
-        })
     }
 }
 </script>
