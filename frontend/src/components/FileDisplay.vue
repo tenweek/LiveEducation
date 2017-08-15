12273d625c5a2796d8eff49062214d195f0c3c40
Wenbin <1352075893@qq.com>
2017-08-15 11:27:53 +0800

    Add dropdown Ref #67

--- frontend/src/components/FileDisplay.vue
+++ frontend/src/components/FileDisplay.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="file-display" id="file" @click="makeFoucs" @keydown="modPicture()" tabindex="0">
         <div class="ppt-header">PPT放映</div>
+        <div class="my-img">
         <img :src="this.route" width="100%" class="my-img" />
+        </div>
         <div class="ppt-footer">
             <div class="for-footer">
         <a class="arrow" :style="this.isTeacher ? 'display:block' : 'display:none'" @click="prePicture"><Icon type="arrow-left-b"></Icon></a>
-        <div class="ppt-num">&nbsp;&nbsp;{{this.nowNum}}&nbsp;of&nbsp;{{this.maxNum}}&nbsp;&nbsp;</div>
+        <select name="cars" @change="changePage" id="for-select" class="for-hover ppt-num" :style="this.isTeacher ? 'display:block' : 'display:none'">
+            <option v-for="num in this.maxNum" class="every-option">&nbsp;{{num}}</option>
+        </select>
+        <div class="ppt-num" :style="this.isTeacher ? 'display:none' : 'display:block'">&nbsp;&nbsp;{{this.nowNum}}</div>
+        <div class="ppt-num">&nbsp;of&nbsp;{{this.maxNum}}&nbsp;&nbsp;</div>
         <a class="arrow" :style="this.isTeacher ? 'display:block' : 'display:none'" @click="nextPicture"><Icon type="arrow-right-b"></Icon></a>
             </div>
         </div>
@@ -32,7 +38,7 @@ export default {
     },
     created: function () {
         this.recRoute = 'ppt/shili-'
-        this.nowNum = 1
+        this.nowNum = 2
         this.maxNum = 15
         if (this.teacherName === this.username) {
             this.isTeacher = true
@@ -67,6 +73,12 @@ export default {
         makeFoucs: function () {
             let getfocus = document.getElementById('file')
             getfocus.focus()
+        },
+        changePage: function () {
+            var mySelect = document.getElementById("for-select")
+            this.nowNum = mySelect.selectedIndex+1
+            this.route = this.baseRoute + this.recRoute + this.nowNum + '.png'
+            this.socket.emit('fileDisplayMessage', this.nowNum, this.roomId + '.2', this.roomId)
         }
     },
     mounted: function () {
@@ -92,6 +104,7 @@ export default {
 <style scoped>
 .my-img {
     border: 3px solid #52524E;
+    height: auto;
 }
 
 .file-display {
@@ -120,6 +133,17 @@ export default {
     font-size: 30px;
     color: white;
     line-height: 80px;
+    background: #52524E;
+    border: none;
+}
+
+.for-hover:hover {
+    color: #9A9B94;
+}
+
+.every-option {
+    background: white;
+    font-size: 10px;
 }
 
 .arrow:hover {
