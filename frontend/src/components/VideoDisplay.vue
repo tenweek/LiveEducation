9c2a1ce905cf84756dbf33b81d565108e4b35a07
tenweek <1511496@mail.nankai.edu.cn>
2017-08-16 15:01:09 +0800

    Delete the callback which only print messages in console Ref #71

--- frontend/src/components/VideoDisplay.vue
+++ frontend/src/components/VideoDisplay.vue
@@ -13,8 +13,8 @@
         <div class="control-panel">
             <button @click="join"><Icon id="join" type="android-arrow-dropright-circle"></Icon></button>
             <template v-if="this.username === this.teacherName">
-                <button @click="publish" id="publish"><Icon type="play"></Icon></button>
-                <button @click="unpublish" id="unpublish"><Icon type="stop"></Icon></button>
+                <button @click="play" id="play"><Icon type="play"></Icon></button>
+                <button @click="stop" id="stop"><Icon type="stop"></Icon></button>
             </template>
             <button @click="showVideo" id="arrow"><Icon type="chevron-up"></Icon></button>
         </div>
@@ -22,7 +22,7 @@
             <template v-if="this.username === this.teacherName">
                 <div id="agora-local"></div>
             </template>
-            <template v-else-if="this.username !== this.teacherName">
+            <template v-else>
                 <div id="agora-remote"></div>
             </template>
         </div>
@@ -64,20 +64,14 @@ export default {
                 arrow.innerHTML = '<i class="ivu-icon ivu-icon-chevron-down"></i>'
             }
         },
-        publish: function () {
-            document.getElementById('unpublish').disable = false
-            document.getElementById('publish').disable = true
-            this.client.publish(this.localStream, function (err) {
-                console.log('Publish local stream error: ' + err)
-            })
+        play: function () {
+            document.getElementById('stop').disable = false
+            document.getElementById('play').disable = true
             this.localStream.enableVideo()
         },
-        unpublish: function () {
-            document.getElementById('publish').disable = false
-            document.getElementById('unpublish').disable = true
-            this.client.unpublish(this.localStream, function (err) {
-                console.log('Unpublish local stream failed' + err)
-            })
+        stop: function () {
+            document.getElementById('play').disable = false
+            document.getElementById('stop').disable = true
             this.localStream.disableVideo()
         },
         join: function () {
@@ -96,7 +90,6 @@ export default {
             this.monitorStream()
         },
         clientInit: function () {
-            console.log('Init AgoraRTC client with vendor key:')
             this.client = AgoraRTC.createClient({ mode: 'interop' })
             this.client.init(this.appKey, () => {
                 this.client.join(null, this.roomId, null, (uid) => {
@@ -114,25 +107,15 @@ export default {
                         this.localStream.setVideoProfile('720p_3')
                         this.localStream.init(() => {
                             this.localStream.play('agora-local')
-                            this.client.publish(this.localStream, function (err) {
-                                console.log('Publish local stream error: ' + err)
-                            })
-                        }, function (err) {
-                            console.log('getUserMedia failed', err)
+                            this.client.publish(this.localStream)
                         })
                     }
-                }, function (err) {
-                    console.log('Join channel failed', err)
                 })
-            }, function (err) {
-                console.log('AgoraRTC client init failed', err)
             })
         },
         monitorStream: function () {
             this.client.on('stream-added', (evt) => {
-                this.client.subscribe(evt.stream, function (err) {
-                    console.log('Subscribe stream failed', err)
-                })
+                this.client.subscribe(evt.stream)
             })
             this.client.on('stream-subscribed', function (evt) {
                 let stream = evt.stream
@@ -146,16 +129,11 @@ export default {
                 let stream = evt.stream
                 if (stream) {
                     stream.stop()
-                    console.log(evt.uid + ' leaved from this channel')
                 }
             })
         },
         leave: function () {
-            this.client.leave(function () {
-                console.log('Leavel channel successfully')
-            }, function (err) {
-                console.log('Leave channel failed', err)
-            })
+            this.client.leave()
         },
         getDevices: function () {
             AgoraRTC.getDevices(function (devices) {
@@ -169,8 +147,6 @@ export default {
                     } else if (device.kind === 'videoinput') {
                         option.text = device.label || 'camera ' + (this.videoSelect.length + 1)
                         this.videoSelect.appendChild(option)
-                    } else {
-                        console.log('Some other kind of source/device: ', device)
                     }
                 }
             })
@@ -201,6 +177,7 @@ button {
     border: none;
     font-size: 20px;
     margin-right: 12px;
+    outline: none;
 }
 
 .control-panel {
